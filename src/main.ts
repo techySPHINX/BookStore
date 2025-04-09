1000d4b66aa16f99c1b8dcacec53c32f13bfb189
Jagan Kumar Hotta <138506773+techySPHINX@users.noreply.github.com>
2025-04-09 19:01:50 +0530

    init : swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('Bookstore API')
+    .setDescription('API documentation for the Bookstore application')
+    .setVersion('1.0')
+    .addBearerAuth() 
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
-bootstrap();
+void bootstrap();
