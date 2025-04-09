c1a0036a041e0f7b8a0a2c674f66608fcb9ed533
Jagan Kumar Hotta <138506773+techySPHINX@users.noreply.github.com>
2025-04-09 20:15:33 +0530

    update : swagger 📄

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { UsersService } from "./users.service";
+import { ApiTags, ApiParam } from "@nestjs/swagger";
 
+@ApiTags("Users")
 @Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get(":id")
+  @ApiParam({ name: "id", description: "ID of the user", example: "1" }) 
   async findById(@Param("id") id: string) {
     return this.usersService.findById(+id);
   }
