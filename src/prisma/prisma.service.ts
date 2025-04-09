93ef0ff46fe45895c82221930f147f7fd5b898ec
Jagan Kumar Hotta <138506773+techySPHINX@users.noreply.github.com>
2025-04-09 20:21:20 +0530

    fix⭐ : prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     super();
   }
