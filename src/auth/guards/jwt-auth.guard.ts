1b4925cfdf086ff9624bc7510b50d785a1fbb02e
Jagan Kumar Hotta <138506773+techySPHINX@users.noreply.github.com>
2025-04-09 19:56:40 +0530

    add : Prisma and Users 🐕‍🦺

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -2,23 +2,24 @@ import { Injectable } from "@nestjs/common";
 import {
   CanActivate,
   ExecutionContext,
+  Inject,
   UnauthorizedException,
 } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
 import { Reflector } from "@nestjs/core";
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
 
-  canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest<{ user?: any }>();
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const user: { id: string; role: string } | undefined = request.user;
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
 
     if (!user) {
       throw new UnauthorizedException();
     }
 
-    return Promise.resolve(true);
+    return true;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Swagger Configuration
   const config = new DocumentBuilder()
-    .setTitle('Bookstore API')
-    .setDescription('API documentation for the Bookstore application')
-    .setVersion('1.0')
+    .setTitle("Bookstore API")
+    .setDescription("API documentation for the Bookstore application")
+    .setVersion("1.0")
     .addBearerAuth() 
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
 
   await app.listen(3000);
 }
-void bootstrap();
+bootstrap();
